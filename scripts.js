098142a4a0883f2c1695cb398585c561cdb38444
Idriss Balijon <I.balijon@gmail.com>
2024-01-22 12:41:46 +0100

    add slider button + comment

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
-carousels = document.querySelectorAll(".carousel");
-console.log(carousels);
+//  Carousel, ma première approche : on affiche les elements qui se trouvent à partir de la seconde place  ( [1] ), 
+// on parcourt le tableau en duplicant le premiere ou le dernier élément au début ou a la fin (direction que l'utilisateur choisi). 
+// notes:
+// !! la taille du tableau doit être au minimum de 3
+// !! overflow sur le conteneur du slider, 
+// !! les éléments doivent être contenu dans un autre conteneur à l'interieur avec une largeur proportionel à la largeur du conteneur parent.
+// !! Définir le nombre d'élément  à afficher par default à l'aide d'une attribut en HTML ?.
+// !! Prendre la distance de l'élément et l'utiliser lors du mouvement du slider.
+
+
+let tabSliderElements = [];
+
+
+document.querySelectorAll(".carousel").forEach(carousel => {
+
+    // Je créé mes boutons pour chaque carousel et je les assigne
+    const buttonSliderLeft = document.createElement('div');
+    const buttonSliderRight = document.createElement('div');
+    buttonSliderLeft.classList.add('slider-button');
+    buttonSliderLeft.classList.add('slider-button-left');
+    buttonSliderRight.classList.add('slider-button');
+    buttonSliderRight.classList.add('slider-button-right');
+    buttonSliderLeft.innerText = '<';
+    buttonSliderRight.innerText = '>';
+    carousel.appendChild(buttonSliderLeft);
+    carousel.appendChild(buttonSliderRight);
+    console.log(carousel);
+});
+
