7c881bb6a8677489342c51a99e458c1f7c0a7a15
Idriss Balijon <I.balijon@gmail.com>
2024-01-23 19:22:49 +0100

    updated carousel to have button "disabled" when at the limit + comments

--- scripts.js
+++ scripts.js
@@ -1,20 +1,11 @@
-// Je vais utiliser une variable index qui sera assossié à l'élément.
-// !! overflow sur le conteneur du slider, 
-// !! les éléments doivent être contenu dans un conteneur .block-carousel à l'interieur du conteneur parent .carousel.
-// !! Prendre la distance de l'élément et l'utiliser lors du mouvement du slider.
-
-// index va nous permettre d'incrémenter et connaitre la position du slider et sera sujet à des conditions.
-
 // on récupére tous éléments avec la class carousel et j'applique le script du click sur les boutons créés
-
-
-
 const carousels = document.querySelectorAll(".carousel");
 
 //J'ai fait en sorte que le script s'adapte si il y a plusieur carousel sur le page, 
-//et est dépandant du nombre d'élément visible par l'utilisateur lors du chargement de la page.
-// ===> !!!!! Le script ne prend en compte les tailles qu'au moment du chargement de la page.
+//et est dépendant du nombre d'élément visible par l'utilisateur lors du chargement de la page.
+// ===> !!!!! Le script ne prend en compte les tailles que au moment du chargement de la page.
 
+// pour chaque élément avec la class carousel que mon querySelector à trouvé on appel la fonction qui mpermet de créer un carousel.
 for (const carousel of carousels) {
     createCarousel(carousel);
 }
@@ -26,8 +17,10 @@ function createCarousel(container) {
     const buttonSliderRight = document.createElement('div');
     buttonSliderLeft.classList.add('slider-button');
     buttonSliderLeft.classList.add('slider-button-left');
+    buttonSliderLeft.classList.add('btn-red');
     buttonSliderRight.classList.add('slider-button');
     buttonSliderRight.classList.add('slider-button-right');
+    buttonSliderRight.classList.add('btn-red');
     buttonSliderLeft.innerText = '<';
     buttonSliderRight.innerText = '>';
     container.parentNode.appendChild(buttonSliderLeft);
@@ -35,37 +28,67 @@ function createCarousel(container) {
 
     // nombre total d'éléments y compris les cachés
     const nmbElements = container.querySelectorAll('.block-carousel > *').length;
+
+    // on montre le bouton de gauch  transparent can il est désactivé
+    buttonDisable(buttonSliderLeft);
+
+    //on met un evenement sur les elements bouton qu'on crée. au click on appel la fonction associer avec les paramètres adapté 
     buttonSliderLeft.addEventListener('click', () => {
-        index = slideToRight(index, false, nmbElements, getNumberOfElementOnDisplay(container), container);
+        index = slideToRight(index, false, nmbElements, container);
     });
     buttonSliderRight.addEventListener('click', () => {
-        index = slideToRight(index, true, nmbElements, getNumberOfElementOnDisplay(container), container);
+        index = slideToRight(index, true, nmbElements, container);
     }
     );
 }
 
+function slideToRight(currentIndex, directionIsRight, nmbElements, container) {
 
-
-
-
-function slideToRight(currentIndex, directionIsRight, nmbElements, nmbDisplayedElements, container) {
+    //on récupére les boutons dans une variable
+    const leftButton = container.parentNode.querySelector('.slider-button-left');
+    const rightButton = container.parentNode.querySelector('.slider-button-right');
 
     if (directionIsRight) {
         // on stop de count quand il y a déja les derniers élements affichés 
-        if (currentIndex < nmbElements - nmbDisplayedElements) {
+        if (currentIndex < nmbElements - getNumberOfElementOnDisplay(container)) {
             currentIndex++;
-
+            // si index est au maximum on change le style du bouton pour montrer que c'est la fin
+            if (currentIndex >= (nmbElements - getNumberOfElementOnDisplay(container))) {
+                buttonDisable(rightButton);
+            } else {
+                // sinon on fait en sorte que le bouton opposé est le bon style.
+                buttonEnable(leftButton);
+            }
         }
-    } else {
         // on stop de decount quand index est déja a 0 ou inférieur.
+    } else {
         if (currentIndex > 0) {
             currentIndex--;
+            // si l'index devient 0 on applique la classe de style qui montre le bouton transparent
+            if (currentIndex == 0) {
+                buttonDisable(leftButton);
 
+            } else {
+                // sinon on fait en sorte que le bouton opposé est le bon style.
+                buttonEnable(rightButton);
+            }
         }
     }
-    moveSlide(currentIndex, container, distanceToSlide(container));
-    return currentIndex;
 
+    // on appel la fonction qui gère le déplacement
+    moveSlide(currentIndex, container, distanceToSlide(container));
+    // je renvoie le nouvel index
+    return currentIndex;
+}
+
+// changer d'apparence des bouton en ajoutant ou en enlevant des class
+function buttonDisable(button) {
+    button.classList.add('btn-disable');
+    button.classList.remove('btn-red');
+}
+function buttonEnable(button) {
+    button.classList.remove('btn-disable');
+    button.classList.add('btn-red');
 }
 
 // j'assume que les éléments ont tous la même taille
@@ -76,6 +99,7 @@ function distanceToSlide(container) {
 }
 function moveSlide(index, container, widthPercentElement) {
 
+    // le pourcentage applique par translate est proportionel a notre index, associer avec la transition dans la class on obtient un mouvement fluid.
     container.querySelector('.block-carousel').style.transform = 'translateX(' + widthPercentElement * -index + '%)';
 
 }
@@ -83,15 +107,16 @@ function moveSlide(index, container, widthPercentElement) {
 //on calcule la largeur en prenant compte les margins
 function getFirstElementTotalSize(container) {
     const containerSize = container.offsetWidth;
-    const blockContainer = container.firstElementChild;
     // premier element du carousel
+    const blockContainer = container.firstElementChild;
+
     const firstElement = blockContainer.firstElementChild;
-    firstElementComputedStyle = window.getComputedStyle(firstElement);
+    const firstElementComputedStyle = window.getComputedStyle(firstElement);
     return elementSize = parseFloat(firstElementComputedStyle.width) +
         parseFloat(firstElementComputedStyle.marginRight);
 
 }
-// on récupére le nombre d'élément supposé être en vue dans le conteneur
+// on récupére le nombre d'élément visible par l'utilisateur en prenant la taille du container .carousel
 function getNumberOfElementOnDisplay(container) {
     return container.offsetWidth / getFirstElementTotalSize(container);
 }
