0d7c134fb28597d28ec90c062eb6793e2a738486
Idriss Balijon <I.balijon@gmail.com>
2024-01-23 15:19:35 +0100

    made index independant + commented

--- scripts.js
+++ scripts.js
@@ -4,8 +4,49 @@
 // !! Prendre la distance de l'élément et l'utiliser lors du mouvement du slider.
 
 // index va nous permettre d'incrémenter et connaitre la position du slider et sera sujet à des conditions.
-// besoin de dissocier cette variable index des différents carousels
-let index = 0;
+
+// on récupére tous éléments avec la class carousel et j'applique le script du click sur les boutons créés
+
+
+
+const carousels = document.querySelectorAll(".carousel");
+
+//J'ai fait en sorte que le script s'adapte si il y a plusieur carousel sur le page, 
+//et est dépandant du nombre d'élément visible par l'utilisateur lors du chargement de la page.
+// ===> !!!!! Le script ne prend en compte les tailles qu'au moment du chargement de la page.
+
+for (const carousel of carousels) {
+    createCarousel(carousel);
+}
+function createCarousel(container) {
+    // index 0 est l'enfant direct du block-carousel dans carousel.
+    let index = 0;
+    // Je créé mes boutons pour chaque carousel et je les assigne
+    const buttonSliderLeft = document.createElement('div');
+    const buttonSliderRight = document.createElement('div');
+    buttonSliderLeft.classList.add('slider-button');
+    buttonSliderLeft.classList.add('slider-button-left');
+    buttonSliderRight.classList.add('slider-button');
+    buttonSliderRight.classList.add('slider-button-right');
+    buttonSliderLeft.innerText = '<';
+    buttonSliderRight.innerText = '>';
+    container.parentNode.appendChild(buttonSliderLeft);
+    container.parentNode.appendChild(buttonSliderRight);
+
+    // nombre total d'éléments y compris les cachés
+    const nmbElements = container.querySelectorAll('.block-carousel > *').length;
+    buttonSliderLeft.addEventListener('click', () => {
+        index = slideToRight(index, false, nmbElements, getNumberOfElementOnDisplay(container), container);
+    });
+    buttonSliderRight.addEventListener('click', () => {
+        index = slideToRight(index, true, nmbElements, getNumberOfElementOnDisplay(container), container);
+    }
+    );
+}
+
+
+
+
 
 function slideToRight(currentIndex, directionIsRight, nmbElements, nmbDisplayedElements, container) {
 
@@ -16,7 +57,7 @@ function slideToRight(currentIndex, directionIsRight, nmbElements, nmbDisplayedE
 
         }
     } else {
-        // on stop de count quand il y a déja les premiers élements affichés
+        // on stop de decount quand index est déja a 0 ou inférieur.
         if (currentIndex > 0) {
             currentIndex--;
 
@@ -54,32 +95,4 @@ function getFirstElementTotalSize(container) {
 function getNumberOfElementOnDisplay(container) {
     return container.offsetWidth / getFirstElementTotalSize(container);
 }
-
-// on récupére tous éléments avec la class carousel et j'applique le script du click sur les boutons créés
-document.querySelectorAll(".carousel").forEach(carousel => {
-
-    // Je créé mes boutons pour chaque carousel et je les assigne
-    const buttonSliderLeft = document.createElement('div');
-    const buttonSliderRight = document.createElement('div');
-    buttonSliderLeft.classList.add('slider-button');
-    buttonSliderLeft.classList.add('slider-button-left');
-    buttonSliderRight.classList.add('slider-button');
-    buttonSliderRight.classList.add('slider-button-right');
-    buttonSliderLeft.innerText = '<';
-    buttonSliderRight.innerText = '>';
-    carousel.parentNode.appendChild(buttonSliderLeft);
-    carousel.parentNode.appendChild(buttonSliderRight);
-
-    // nombre total d'éléments y compris les cachés
-    const nmbElements = carousel.querySelectorAll('.block-carousel > *').length;
-    buttonSliderLeft.addEventListener('click', () => {
-        index = slideToRight(index, false, nmbElements, getNumberOfElementOnDisplay(carousel), carousel);
-    });
-    buttonSliderRight.addEventListener('click', () => {
-        index = slideToRight(index, true, nmbElements, getNumberOfElementOnDisplay(carousel), carousel);
-    }
-    );
-}
-
-);
 
